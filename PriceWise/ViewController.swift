f59323c74830427cf2a24007cba3406cdbb4adb3
iamMarlonNoble <marlon.noble94@gmail.com>
2023-11-30 14:25:52 +0800

    test 2

--- PriceWise/ViewController.swift
+++ PriceWise/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     var testOnly = "This is a test only"
+    
+    
+    var testOnly2 = "This is a test only"
 
     override func viewDidLoad() {
         super.viewDidLoad()
